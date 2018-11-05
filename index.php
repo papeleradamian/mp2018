c9a41d4e77a368850cc40dcfa45507589abe45c1
Damian Gabriel Torres <42480571+papeleradamian@users.noreply.github.com>
2018-11-05 17:34:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,18 @@
     MercadoPago\SDK::setClientSecret("N9WwN6Ru7ke9cBAS7F1ITtUCOToNgrKN");
     MercadoPago\SDK::setAccessToken("TEST-8131526712381965-100207-ecaccd3929d0adac787444c261282027-66839567");
     
-// SDK Version >= 0.5.0
+$payment = new MercadoPago\Payment();
 
-$mp = new MP("ACCESS_TOKEN");
-
-$request = array(
-        "uri" => "/v1/account/settlement_report",
-        "data" => array(
-            "begin_date" => "2018-11-01T00:00:00Z",
-            "end_date" => "2018-11-04T00:00:00Z"
-        )
+    $payment->transaction_amount = 141;
+    $payment->token = "YOUR_CARD_TOKEN";
+    $payment->description = "Ergonomic Silk Shirt";
+    $payment->installments = 1;
+    $payment->payment_method_id = "visa";
+    $payment->payer = array(
+      "email" => "larue.nienow@hotmail.com"
     );
 
-$mp->post($request);
+    $payment->save();
 
+    echo $payment->status;
   ?>
