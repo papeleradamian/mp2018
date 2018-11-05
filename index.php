8206752333f392d3d2065d8d3f236dcf5fc58ed0
Damian Gabriel Torres <42480571+papeleradamian@users.noreply.github.com>
2018-11-05 12:04:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
-composer require "mercadopago/dx-php:dev-master"
- require __DIR__  . '/vendor/autoload.php';
+<?php
+
+    require_once 'vendor/autoload.php';
+
+    MercadoPago\SDK::setAccessToken("TEST-6883424594190986-071506-05714c036faa6ff245e70a42dba20d92__LB_LC__-66839567");
+
+    $payment = new MercadoPago\Payment();
+
+    $payment->transaction_amount = 141;
+    $payment->token = "YOUR_CARD_TOKEN";
+    $payment->description = "Ergonomic Silk Shirt";
+    $payment->installments = 1;
+    $payment->payment_method_id = "visa";
+    $payment->payer = array(
+      "email" => "larue.nienow@hotmail.com"
+    );
+
+    $payment->save();
+
+    echo $payment->status;
+
+  ?>
