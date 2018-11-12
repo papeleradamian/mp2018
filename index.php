ae713240bf7f055f5cacaec4554a585aeb3dbc5f
Damian Gabriel Torres <42480571+papeleradamian@users.noreply.github.com>
2018-11-12 17:40:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
     
     MercadoPago\SDK::setClientId("8131526712381965");
     MercadoPago\SDK::setClientSecret("N9WwN6Ru7ke9cBAS7F1ITtUCOToNgrKN");
-    MercadoPago\SDK::setAccessToken("TEST-8131526712381965-100207-ecaccd3929d0adac787444c261282027-66839567");
+    MercadoPago\SDK::setAccessToken("APP_USR-8131526712381965-111216-5be425ff72d5a9d7a55caf06bac51a18-66839567");
     
 $request =
 
-$mp = new MP("TEST-8131526712381965-100207-ecaccd3929d0adac787444c261282027-66839567");
+$mp = new MP("APP_USR-8131526712381965-111216-5be425ff72d5a9d7a55caf06bac51a18-66839567");
 
 $request = array(
         "uri" => "/v1/account/bank_report",
         "data" => array(
-            "begin_date" => "2015-05-01T00:00:00Z",
-            "end_date" => "2015-06-01T00:00:00Z"
+            "begin_date" => "2018-05-01T00:00:00Z",
+            "end_date" => "2018-06-01T00:00:00Z"
         )
     );
 
